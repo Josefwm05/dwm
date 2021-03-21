948d5efa278f0b1322c41fe1d5c8c9fb826a3526
Josef Wren Menad <josefwm05@gmail.com>
2021-03-21 09:19:01 +0100

    fixed an error

--- config.def.h
+++ config.def.h
@@ -27,7 +27,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "NULL,      NULL,       NULL,       0,            FALSE,       -1 },
+	{ NULL,       NULL,       NULL,       0,            False,       -1 },
 };
 
 /* layout(s) */
